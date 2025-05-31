c1bb087ff8c529c56f0e21cf922004782187fa6d
bobcbui <38550226+bobcbui@users.noreply.github.com>
2025-05-31 07:40:45 +0000

    Deploying to main from @ bobcbui/bobcbui@9ec25ef7371c07206829419dd87f442cf96d2267 🚀

--- docs/.vuepress/.temp/internal/siteData.js
+++
@@ -1,1 +0,0 @@
-export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"牛马程序员\",\"description\":\"为了帮助更多企业培养和便宜好用高效、实用的程序员，我们一直在不断努力培养出成千上万的牛马程序员。\",\"head\":[],\"locales\":{}}")
--- docs/.vuepress/.temp/pages/404.html.js
+++
@@ -1,3 +0,0 @@
-import comp from "D:/AProject/bobcbui/docs/.vuepress/.temp/pages/404.html.vue"
-const data = JSON.parse("{\"path\":\"/404.html\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{\"layout\":\"NotFound\"},\"headers\":[],\"git\":{},\"filePathRelative\":null}")
-export { comp, data }
