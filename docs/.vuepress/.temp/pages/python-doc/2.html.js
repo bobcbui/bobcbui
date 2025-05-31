9208ed665b7f463dd455b2f62fddcbaaca754031
Bobcbui <bobcbui@outlook.com>
2025-05-31 16:29:17 +0800

    111

---
+++ docs/.vuepress/.temp/pages/python-doc/2.html.js
@@ -0,0 +1,16 @@
+import comp from "D:/AProject/bobcbui/docs/.vuepress/.temp/pages/python-doc/2.html.vue"
+const data = JSON.parse("{\"path\":\"/python-doc/2.html\",\"title\":\"关于牛马程序员\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"我们的使命\",\"slug\":\"我们的使命\",\"link\":\"#我们的使命\",\"children\":[]},{\"level\":2,\"title\":\"我们的价值观\",\"slug\":\"我们的价值观\",\"link\":\"#我们的价值观\",\"children\":[]},{\"level\":2,\"title\":\"联系我们\",\"slug\":\"联系我们\",\"link\":\"#联系我们\",\"children\":[]}],\"git\":{},\"filePathRelative\":\"python-doc/2.md\"}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
